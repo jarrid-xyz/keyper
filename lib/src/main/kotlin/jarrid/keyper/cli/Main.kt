4ee2d060fc1b7258cdaa869ef716266a130002e6
Lulu Cheng <l1990790120@gmail.com>
2024-07-07 02:05:51 -0400

    [change] restructure cli slightly

--- lib/src/main/kotlin/jarrid/keyper/cli/Main.kt
+++ lib/src/main/kotlin/jarrid/keyper/cli/Main.kt
@@ -1,20 +1,29 @@
 package jarrid.keyper.cli
 
 import com.github.ajalt.clikt.core.subcommands
+import jarrid.keyper.cli.data.Encrypt
 import jarrid.keyper.cli.deploy.Apply
 import jarrid.keyper.cli.deploy.Destroy
 import jarrid.keyper.cli.deploy.Plan
+import jarrid.keyper.cli.key.Create
+import jarrid.keyper.cli.key.List
 import jarrid.keyper.utils.logging.config
 
 fun main(args: Array<String>) {
     config()
     Keyper()
         .subcommands(
-            Key(),
+            Key().subcommands(
+                Create(),
+                List()
+            ),
             Deploy().subcommands(
                 Plan(),
                 Apply(),
                 Destroy()
+            ),
+            Data().subcommands(
+                Encrypt()
             )
         ).main(args)
 }
