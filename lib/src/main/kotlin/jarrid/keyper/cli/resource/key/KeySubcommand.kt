b696f6d8831b491f2c96e4b53ecc8e89c90ae27c
Lulu Cheng <l1990790120@gmail.com>
2024-08-08 02:34:31 -0400

    [change] refactor encrypt/decrypt

--- lib/src/main/kotlin/jarrid/keyper/cli/resource/key/KeySubcommand.kt
+++ lib/src/main/kotlin/jarrid/keyper/cli/resource/key/KeySubcommand.kt
@@ -3,14 +3,18 @@ package jarrid.keyper.cli.resource.key
 import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
 import jarrid.keyper.cli.BaseSubcommand
 import jarrid.keyper.resource.Deployment
 import jarrid.keyper.utils.file.Backend
 import jarrid.keyper.utils.model.toUUID
 import kotlinx.coroutines.runBlocking
+import java.io.File
 import java.util.*
 import jarrid.keyper.resource.key.Manager as KeyManager
 import jarrid.keyper.resource.key.Model as Key
+
+class InputValidationException(message: String = "") : Exception(message)
 
 
 abstract class KeySubcommand(help: String = "") : BaseSubcommand(help = help) {
@@ -25,6 +29,9 @@ abstract class KeySubcommand(help: String = "") : BaseSubcommand(help = help) {
     private val keyName: String? by option(
         "-n", "--key-name", help = "Key name to use"
     )
+
+    val inputPath: File? by option(help = "The input file path containing plaintext data to encrypt").file()
+    val outputPath: File? by option(help = "The output file path containing ciphertext").file()
 
     private lateinit var useBackend: Backend
     lateinit var useDeployment: Deployment
@@ -44,6 +51,16 @@ abstract class KeySubcommand(help: String = "") : BaseSubcommand(help = help) {
         runBlocking {
             key = keyManager.getKey(keyId, keyName, useDeployment)
             runAsync()
+        }
+    }
+
+    fun validate(input: String?) {
+        if (input != null && inputPath != null) {
+            throw InputValidationException("You must specify either plaintext or filePath, not both")
+        }
+
+        if (input == null && inputPath == null) {
+            throw InputValidationException("You must specify either plaintext or filePath")
         }
     }
 
--- lib/src/main/kotlin/jarrid/keyper/resource/key/data/Base.kt
+++ lib/src/main/kotlin/jarrid/keyper/resource/key/data/Base.kt
@@ -1,12 +1,16 @@
 package jarrid.keyper.resource.key.data
 
 import com.google.cloud.kms.v1.CryptoKeyName
+import com.google.protobuf.ByteString
 import io.klogging.Klogging
 import jarrid.keyper.app.Backend
 import jarrid.keyper.app.Config
 import jarrid.keyper.app.Stack
 import jarrid.keyper.resource.Model
 import jarrid.keyper.resource.key.Name
+import java.io.File
+import java.nio.file.Files
+import java.util.*
 
 abstract class Base(
     val backend: Backend,
@@ -18,6 +22,39 @@ abstract class Base(
     val provider = stack.getConfig(app)
     private val projectId = provider.accountId
     private val region = provider.region
+    val keyName = getKeyName(key)
+
+    fun run(input: String): String {
+        return input.toByteString().run().toStringUtf8()
+    }
+
+    fun run(input: String, output: File) {
+        input.toByteString().run().write(output)
+    }
+
+    fun run(input: File): String {
+        return input.toByteString().run().toStringUtf8()
+    }
+
+    fun run(input: File, output: File) {
+        input.toByteString().run().write(output)
+    }
+
+    fun ByteString.write(output: File) {
+        Files.write(output.toPath(), Base64.getEncoder().encode(this.toByteArray()))
+    }
+
+    fun String.toByteString(): ByteString {
+        return ByteString.copyFrom(Base64.getDecoder().decode(this))
+    }
+
+    fun File.toByteString(): ByteString {
+        return ByteString.copyFrom(Base64.getDecoder().decode(Files.readAllBytes(this.toPath())))
+    }
+
+    fun ByteString.toBase64String(): String {
+        return Base64.getEncoder().encodeToString(this.toByteArray())
+    }
 
     fun getKeyName(key: Model): CryptoKeyName {
         return CryptoKeyName.of(
@@ -27,4 +64,6 @@ abstract class Base(
             Name.getJarridKeyName(key.resource.base)
         )
     }
+
+    abstract fun ByteString.run(): ByteString
 }
