546b6aee567f80ecd8ff8dca8bc5cef1b73cfa49
Lulu Cheng <l1990790120@gmail.com>
2024-09-14 00:06:25 -0400

    [try] remove printout format

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 
-# Run the Java command and capture its output
-output=$(java -jar /home/keyper/lib/build/libs/lib-cli.jar "$@")
-
-# Print the output, replacing newlines with spaces
-echo "$output" | tr '\n' ' '
-
-# Add a final newline for better formatting
-echo
+# Run the Java command and output its result directly
+java -jar /home/keyper/lib/build/libs/lib-cli.jar "$@"
