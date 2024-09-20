1ab9dff50c69557b0d0c02a5ebb93b7571672e85
Lulu Cheng <l1990790120@gmail.com>
2024-09-20 19:40:46 -0400

    [revert] seems to be working, make it run on main again

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 output=$(java -jar /home/keyper/lib/build/libs/lib-cli.jar "$@")
 
 # Set the output using GitHub Actions Workflow Commands
-echo "## Keyper Action Output" >> $GITHUB_STEP_SUMMARY
+echo "## Keyper Command Output" >> $GITHUB_STEP_SUMMARY
 echo "$output" >> $GITHUB_STEP_SUMMARY
 
 # Output to console as well (this will be visible in the action logs)
