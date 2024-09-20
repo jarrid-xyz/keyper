a2cb932e92a46b846cf6458380d5dcbf4fbab755
Lulu Cheng <l1990790120@gmail.com>
2024-09-20 02:17:46 -0400

    [change] add github action brand and stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
 
-# Run the Java command and output its result, wrapping each line with GitHub Actions syntax
-java -jar /home/keyper/lib/build/libs/lib-cli.jar "$@" | while IFS= read -r line; do
-    echo "::echo::$line"
-done
+# Capture the output of the Java command
+output=$(java -jar /home/keyper/lib/build/libs/lib-cli.jar "$@")
+
+# Set the output using GitHub Actions Workflow Commands
+echo "stdout<<EOF" >> $GITHUB_OUTPUT
+echo "$output" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
+
+# Output to console as well (this will be visible in the action logs)
+echo "$output"
+
+# Exit with the same status as the Java command
+exit $?
