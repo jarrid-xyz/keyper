ca5c47f933d87b1377331ac2668e3bc2dde647d3
Lulu Cheng <l1990790120@gmail.com>
2024-09-22 18:36:15 -0400

    [try] seems to be working locally but giving a diff error

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -3,15 +3,18 @@ set -e
 
 ADDR=("$@")
 
-echo "## Keyper Command Output" >> $GITHUB_STEP_SUMMARY
+LOG_FILE="/home/keyper/keyper.log"
 
 # Capture stdout and stderr from the Keyper command
 # Set the output using GitHub Actions Workflow Commands
-java -jar /home/keyper/lib/build/libs/lib-cli.jar "${ADDR[@]}" &> $GITHUB_STEP_SUMMARY
+java -jar /home/keyper/lib/build/libs/lib-cli.jar "${ADDR[@]}" &> $LOG_FILE || true
 status=$?
 
+echo "## Keyper Command Output" >> $GITHUB_STEP_SUMMARY
+cat $LOG_FILE >> $GITHUB_STEP_SUMMARY
+
 # Output to console as well (this will be visible in the action logs)
-cat $GITHUB_STEP_SUMMARY
+cat $LOG_FILE
 
 # Exit with the same status as the Keyper command
 exit $status
