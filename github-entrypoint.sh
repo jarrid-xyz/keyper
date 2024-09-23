bccdd0eef301c2b3a9e48e641fd16943394115ea
Lulu Cheng <l1990790120@gmail.com>
2024-09-23 16:16:15 -0400

    [try] add copy in two places let's see

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -e
 
-export IS_DOCKER=true
+export IS_GITHUB_ACTION=true
 
 IFS=' ' read -r -a ADDR <<< "$@"
 
 LOG_FILE="/home/keyper/keyper.log"
-CDKTF_JSON_FILE="/home/keyper/cdktf.json"
 
-# Update the "app" line in /home/keyper/cdktf.json
-sed -i 's#"app": "java -jar lib/build/libs/lib-main.jar"#"app": "java -jar /home/keyper/lib/build/libs/lib-main.jar"#' $CDKTF_JSON_FILE
-
-# Copy the updated file to /github/workspace/cdktf.json
-cp $CDKTF_JSON_FILE /github/workspace/cdktf.json
-
-echo "Updated and copied cdktf.json files"
-
-# Debug: Print contents of cdktf.json
-echo "DEBUG: Contents of $CDKTF_JSON_FILE:"; cat $CDKTF_JSON_FILE
-echo "DEBUG: Contents of /github/workspace/cdktf.json:"; cat /github/workspace/cdktf.json
+# NOTE:Maybe we don't need to copy the files to the workspace, giving it a try
+cp /home/keyper/cdktf.json /home/keyper/cdktf-app.sh /github/workspace
+echo "DEBUG: Github Workspace Directory: $(ls -la /github/workspace)"
 
 # Capture stdout and stderr from the Keyper command
 java -jar /home/keyper/lib/build/libs/lib-cli.jar "${ADDR[@]}" &> $LOG_FILE || status=$?
