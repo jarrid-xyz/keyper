1ad135924f16f4f7d97623e14cbb1e5d54a223a5
Lulu Cheng <l1990790120@gmail.com>
2024-09-22 21:59:25 -0400

    [fix] we need to add this file manually for github action specifically

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -5,13 +5,28 @@ IFS=' ' read -r -a ADDR <<< "$@"
 
 LOG_FILE="/home/keyper/keyper.log"
 
-# Update app path in cdktf.json, this is only needed in github action
-sed -i 's|"app": "java -jar lib/build/libs/lib-main.jar"|"app": "java -jar /home/keyper/lib/build/libs/lib-main.jar"|' cdktf.json
+
+# Create cdktf.json file, this is only needed in github action
+cat << EOF > cdktf.json
+{
+  "language": "java",
+  "app": "java -jar /home/keyper/lib/build/libs/lib-main.jar",
+  "projectId": "25a6a20e-00d6-4ac9-9388-7f3954f7db71",
+  "sendCrashReports": "true",
+  "codeMakerOutput": "imports",
+  "terraformProviders": [],
+  "terraformModules": [],
+  "context": {
+  }
+}
+EOF
+
+echo "Created cdktf.json file"
 
 # Capture stdout and stderr from the Keyper command
 java -jar /home/keyper/lib/build/libs/lib-cli.jar "${ADDR[@]}" &> $LOG_FILE || status=$?
 
-# Set the output using GitHub Actions Workflow Commands
+# Set github action output
 echo "## Keyper Command Output" >> $GITHUB_STEP_SUMMARY
 cat $LOG_FILE >> $GITHUB_STEP_SUMMARY
 
