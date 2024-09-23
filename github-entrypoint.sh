0cc1dd76bdaca3b39d7c3b9325aa1d25bc2b3f05
Lulu Cheng <l1990790120@gmail.com>
2024-09-23 00:39:36 -0400

    [try]

--- github-entrypoint.sh
+++ github-entrypoint.sh
@@ -4,31 +4,29 @@ set -e
 IFS=' ' read -r -a ADDR <<< "$@"
 
 LOG_FILE="/home/keyper/keyper.log"
+CDKTF_JSON_FILE="/home/keyper/cdktf.json"
 
+# Update the "app" line in /home/keyper/cdktf.json
+sed -i 's#"app": "java -jar lib/build/libs/lib-main.jar"#"app": "java -jar /home/keyper/lib/build/libs/lib-main.jar"#' $CDKTF_JSON_FILE
 
-# Create cdktf.json file, this is only needed in github action
-cat << EOF > /home/keyper/cdktf.json
-{
-  "language": "java",
-  "app": "java -jar /home/keyper/lib/build/libs/lib-main.jar",
-  "projectId": "25a6a20e-00d6-4ac9-9388-7f3954f7db71",
-  "sendCrashReports": "true",
-  "codeMakerOutput": "imports",
-  "terraformProviders": [],
-  "terraformModules": [],
-  "context": {
-  }
-}
-EOF
+# Copy the updated file to /github/workspace/cdktf.json
+cp $CDKTF_JSON_FILE /github/workspace/cdktf.json
 
-echo "Created cdktf.json file"
+echo "Updated and copied cdktf.json files"
+
+# Debug: Print contents of cdktf.json
+echo "DEBUG: Contents of $CDKTF_JSON_FILE:"; cat $CDKTF_JSON_FILE
+echo "DEBUG: Contents of /github/workspace/cdktf.json:"; cat /github/workspace/cdktf.json
 
 # Capture stdout and stderr from the Keyper command
 java -jar /home/keyper/lib/build/libs/lib-cli.jar "${ADDR[@]}" &> $LOG_FILE || status=$?
 
 # Set github action output
 echo "## Keyper Command Output" >> $GITHUB_STEP_SUMMARY
+# Format Keyper log in code block
+echo '```' >> $GITHUB_STEP_SUMMARY
 cat $LOG_FILE >> $GITHUB_STEP_SUMMARY
+echo '```' >> $GITHUB_STEP_SUMMARY
 
 # Output to console as well (this will be visible in the action logs)
 cat $LOG_FILE
