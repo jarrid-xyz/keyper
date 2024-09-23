bdca7a3ddb3b8058c89f29e2086327bb489aa453
Lulu Cheng <l1990790120@users.noreply.github.com>
2024-09-23 17:06:06 -0400

    [try] add copy in two places let's see (#59)

--- cdktf-app.sh
+++ cdktf-app.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-if [ "$IS_DOCKER" = "true" ]; then
+if [ "$IS_GITHUB_ACTION" = "true" ]; then
+  # NOTE: Maybe we don't need to copy the files to the workspace, giving it a try
+  cp /home/keyper/cdktf.json /home/keyper/cdktf-app.sh /github/workspace/
   java -jar /home/keyper/lib/build/libs/lib-main.jar
 else
   java -jar lib/build/libs/lib-main.jar
